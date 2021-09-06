62a558b11a0af36f1e3408c775653325288b73ea
Tim Marchant <fromtimwithcode@gmail.com>
2021-09-06 16:54:48 -0500

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = props => {
+    return (
+        <div className="header">
+            <h1>Todoizt</h1>
+        </div>
+    )
+};
+
+export default Header;
